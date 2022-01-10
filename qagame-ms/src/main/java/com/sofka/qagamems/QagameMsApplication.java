beb5c7288df4e87c2ef8656e16f27a77b222fbb1
Jhonny Castro Clavijo <johnnycastro72@msn.com>
2022-01-09 22:03:23 -0500

    initial qagame ms commit

---
+++ qagame-ms/src/main/java/com/sofka/qagamems/QagameMsApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.qagamems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QagameMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QagameMsApplication.class, args);
+	}
+
+}
---
+++ qagame-ms/src/test/java/com/sofka/qagamems/QagameMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.qagamems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QagameMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
