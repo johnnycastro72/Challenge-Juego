5a3d1aa01fed727b2615e2ba2967bdcba36cde97
Jhonny Castro Clavijo <johnnycastro72@msn.com>
2022-01-11 16:27:25 -0500

    Resolvers implemented

---
+++ qagame-api/src/resolvers/index.js
@@ -0,0 +1,14 @@
+const authResolver = require('./auth_resolver');
+const categoryResolver = require('./category_resolver');
+const optionResolver = require('./option_resolver');
+const prizeResolver = require('./prize_resolver');
+const questionResolver = require('./question_resolver');
+const recordResolver = require('./record_resolver');
+const roundResolver = require('./round_resolver');
+const statusResolver = require('./status_resolver');
+
+const lodash = require('lodash');
+
+const resolvers = lodash.merge(authResolver, categoryResolver, optionResolver, prizeResolver, questionResolver, recordResolver, roundResolver, statusResolver);
+
+module.exports = resolvers;
